52c21d513d48a02e3e9475540e3f17289b27ca8a
Vinay Kumar <vinayjarang22196@gmail.com>
2021-03-27 18:43:40 +0530

    LoginPageTest Changes

--- PageObjectModel/src/test/java/com/spicejet/qa/testcases/LoginPageTest.java
+++ PageObjectModel/src/test/java/com/spicejet/qa/testcases/LoginPageTest.java
@@ -4,8 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.NoInjection;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import com.spicejet.qa.base.TestBase;
